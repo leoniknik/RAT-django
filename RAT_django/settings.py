302e88af6c0687ff38e51827938ca6cc6b5419a9
leoniknik <leoniknik@mail.ru>
2017-04-19 01:55:49 +0300

    add deploy files

--- RAT_django/settings.py
+++ RAT_django/settings.py
@@ -73,6 +73,15 @@ WSGI_APPLICATION = 'RAT_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'rat',
+#        'USER': 'root',
+#        'PASSWORD': '1234',
+#    }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
