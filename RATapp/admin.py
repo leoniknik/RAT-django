7886c37fb8f8a62a7e254ebad65e4d0607e8bde9
leoniknik <leoniknik@mail.ru>
2017-05-28 14:53:41 +0300

    add admin site

--- RATapp/admin.py
+++ RATapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Vehicle, Crash, CrashDescription, Service, Offer, HighOffer, LowOffer, Review
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'firstname', 'lastname', 'phone',)
+
+
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = ('VIN', 'number', 'brand', 'model', 'year', 'user', 'is_auction')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
