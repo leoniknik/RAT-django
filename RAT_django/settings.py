613624dd693054ddd22755e78b4ddd262a8c5067
leoniknik <leoniknik@mail.ru>
2017-05-28 16:44:51 +0300

    new static root

--- RAT_django/settings.py
+++ RAT_django/settings.py
@@ -136,8 +136,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 AUTH_USER_MODEL = 'RATapp.User'
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
